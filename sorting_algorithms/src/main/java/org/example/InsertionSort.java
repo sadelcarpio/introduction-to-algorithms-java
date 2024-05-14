40008c6c02c3ee0fe5c2f899daf0bd9a191eca97
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-14 13:14:21 -0500

    Parameter never used fix

--- sorting_algorithms/src/main/java/org/example/InsertionSort.java
+++ sorting_algorithms/src/main/java/org/example/InsertionSort.java
@@ -17,7 +17,7 @@ public class InsertionSort {
     }
 
     public static void sort(int[] A, int n) {
-        for (int i = 1; i < A.length; i++) {
+        for (int i = 1; i < n; i++) {
             int key = A[i];
             int j = i - 1;
             while (j >= 0 && A[j] > key) {
