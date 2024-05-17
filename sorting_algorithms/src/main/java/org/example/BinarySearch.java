e6c7000a025f1b8c82c7dbe3c559e5e00a77bef9
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-17 00:44:02 -0500

    Binary Search

---
+++ sorting_algorithms/src/main/java/org/example/BinarySearch.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = new int[]{6, 5, 7, 3, 2, 1};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int index = binarySearch(arr, 4, 0, arr.length);
+        System.out.println(index);
+    }
+
+    private static int binarySearch(int[] a, int val, int p, int r) {
+        if (p >= r) return -1;
+        int q = (p + r) / 2;
+        if (val == a[q]) return q;
+        if (val < a[q]) {
+            return binarySearch(a, val, p, q);
+        }
+        return binarySearch(a, val, q + 1, r);
+    }
+}
